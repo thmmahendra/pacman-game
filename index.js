4a771c1afedd5ecf060350608c72f141b9bb8772
thmmahendra <thmmahendra@gmail.com>
2024-01-18 22:26:12 +0545

    Initail Commit

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+class Boundary {
+    constructor({ position }) {
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+    }
+}
+
+const boundary = new Boundary({
+    position: {
+        x: 0,
+        y: 0
+    }
+})
+
+boundary.draw()
