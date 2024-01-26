b09972ef4ef62b966ccc2695ecfebf8da23c0267
thmmahendra <thmmahendra@gmail.com>
2024-01-26 22:35:14 +0545

    Intial Commits

--- index.js
+++ index.js
@@ -47,6 +47,28 @@ class Player {
     }
 }
 
+class Ghost {
+    constructor({ position, velocity, color = 'red' }) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+        this.color = color
+        this.prevCollisions = []
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = this.color
+        c.fill()
+        c.closePath()
+    }
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 class Pellet {
     constructor({ position }) {
         this.position = position
@@ -63,6 +85,19 @@ class Pellet {
 
 const pellets = []
 const boundries = []
+const ghosts = [
+    new Ghost({
+        position: {
+            x: Boundary.width * 6 + Boundary.width / 2,
+            y: Boundary.height + Boundary.height / 2
+        },
+        velocity: {
+            x: 5,
+            y: 0
+        }
+    })
+]
+
 const player = new Player({
     position: {
         x: Boundary.width + Boundary.width / 2,
@@ -452,8 +487,91 @@ function animate() {
     })
 
     player.update()
-    // player.velocity.x = 0
-    // player.velocity.y = 0
+
+    ghosts.forEach(ghost => {
+        ghost.update()
+
+        const collisions = []
+        boundries.forEach(boundary => {
+            if (
+                !collisions.includes('right') &&
+                circleCollideWithRectangle({
+                    circle: {
+                        ...ghost, velocity: {
+                            x: 5,
+                            y: 0
+                        }
+                    },
+                    rectangle: boundary
+                })
+            ) {
+                collisions.push('right')
+            }
+            if (
+                !collisions.includes('left') &&
+                circleCollideWithRectangle({
+                    circle: {
+                        ...ghost, velocity: {
+                            x: -5,
+                            y: 0
+                        }
+                    },
+                    rectangle: boundary
+                })
+            ) {
+                collisions.push('left')
+            }
+            if (
+                !collisions.includes('up') &&
+                circleCollideWithRectangle({
+                    circle: {
+                        ...ghost, velocity: {
+                            x: 0,
+                            y: -5
+                        }
+                    },
+                    rectangle: boundary
+                })
+            ) {
+                collisions.push('up')
+            }
+            if (
+                !collisions.includes('down') &&
+                circleCollideWithRectangle({
+                    circle: {
+                        ...ghost, velocity: {
+                            x: 0,
+                            y: 5
+                        }
+                    },
+                    rectangle: boundary
+                })
+            ) {
+                collisions.push('down')
+            }
+        })
+
+        if (collisions.length > ghost.prevCollisions.length)
+            ghost.prevCollisions = collisions
+
+        if (JSON.stringify(collisions) != JSON.stringify(ghost.prevCollisions)) {
+            // console.log('gogo')
+
+            if (ghost.velocity.x > 0) ghost.prevCollisions.push('right')
+            else if (ghost.velocity.x < 0) ghost.prevCollisions.push('left')
+            else if (ghost.velocity.y < 0) ghost.prevCollisions.push('up')
+            else if (ghost.velocity.y > 0) ghost.prevCollisions.push('down')
+
+            console.log(collisions)
+            console.log(ghost.prevCollisions)
+
+            const pathways = ghost.prevCollisions.filter((collision) => {
+                return !collisions.includes(collision)
+            })
+            console.log({ pathways })
+        }
+        // console.log(collisions)
+    })
 }
 
 animate()
